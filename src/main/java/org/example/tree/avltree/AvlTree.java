88b79e91fdf25c03fd161355725abfadedd94c67
DESKTOP-E22R50E\Sanqer <daredevilis25@gmail.com>
2020-05-06 03:47:40 +0300

    added avl insert and remove

--- src/main/java/org/example/tree/avltree/AvlTree.java
+++ src/main/java/org/example/tree/avltree/AvlTree.java
@@ -1,5 +1,6 @@
 package org.example.tree.avltree;
 
+import javafx.util.Pair;
 import org.example.tree.TreeContainer;
 import java.util.List;
 import java.util.Stack;
@@ -71,5 +72,101 @@ public class AvlTree<E extends Comparable<E>> implements TreeContainer<E>
     @Override
     public boolean isEmpty() {
         return root == null;
+    }
+
+    private int height(Node<E> node) {
+        return (node != null) ? node.getHeight() : 0;
+    }
+
+    private void fixHeight(Node<E> current) {
+        int hl = height(current.getLeft());
+        int hr = height(current.getRight());
+        current.setHeight(Math.max(hl, hr) + 1);
+    }
+
+    private int getBalance(Node<E> current) {
+        int hl = height(current.getLeft());
+        int hr = height(current.getRight());
+        return hr - hl;
+    }
+
+    private Node<E> rotateRight(Node<E> current) {
+        Node<E> left = current.getLeft();
+        current.setLeft(left.getRight());
+        left.setRight(current);
+        fixHeight(current);
+        fixHeight(left);
+        return left;
+    }
+
+    private Node<E> rotateLeft(Node<E> current) {
+        Node<E> right = current.getRight();
+        current.setRight(right.getLeft());
+        right.setLeft(current);
+        fixHeight(current);
+        fixHeight(right);
+        return right;
+    }
+
+    private Node<E> balance(Node<E> current) {
+        fixHeight(current);
+        if (getBalance(current) == 2) {
+            if (getBalance(current.getRight()) < 0) {
+                current.setRight(rotateRight(current.getRight()));
+            }
+            return rotateLeft(current);
+        }
+        if (getBalance(current) == -2) {
+            if (getBalance(current.getLeft()) > 0) {
+                current.setLeft(rotateLeft(current.getLeft()));
+            }
+            return rotateRight(current);
+        }
+        return current;
+    }
+
+    private Node<E> insert(Node<E> current, E element) throws Exception
+    {
+        if (current == null) return new Node<>(element);
+        int compRes = element.compareTo(current.getKey());
+        if (compRes == 0) throw new Exception("Элемент уже есть в дереве");
+        if (compRes > 0) {
+            current.setRight(insert(current.getRight(), element));
+        } else {
+            current.setLeft(insert(current.getLeft(), element));
+        }
+        return balance(current);
+    }
+
+    private Node<E> findMin(Node<E> current) {
+        return (current.getLeft() != null) ? findMin(current.getLeft()) : current;
+    }
+
+    private Node<E> removeMin(Node<E> current) {
+        if (current.getLeft() == null) {
+            return current.getRight();
+        }
+        current.setLeft(removeMin(current.getLeft()));
+        return balance(current);
+    }
+
+    private Node<E> remove(Node<E> current, E element) {
+        if (current == null) return null;
+        int compRes = element.compareTo(current.getKey());
+        if (compRes > 0) {
+            current.setRight(remove(current.getRight(), element));
+        } else if (compRes < 0) {
+            current.setLeft(remove(current.getLeft(), element));
+        } else { // element == current.getKey()
+            Node<E> left = current.getLeft();
+            Node<E> right = current.getRight();
+            current = null;
+            if (right == null) return left;
+            Node <E> min = findMin(right);
+            min.setRight(removeMin(right));
+            min.setLeft(left);
+            return balance(min);
+        }
+        return balance(current);
     }
 }
