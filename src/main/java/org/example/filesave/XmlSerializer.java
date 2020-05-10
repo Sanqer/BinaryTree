803c917dacf0cf3d95c81eb325195d102f4bf194
DESKTOP-E22R50E\Sanqer <daredevilis25@gmail.com>
2020-05-10 15:16:00 +0300

    added XmlSerializer, added more tests, made Sex from boolean to enum, added configure method to TreeContainer
    (cherry picked from commit 2dd41ad297c24bbd19dbb7097da1fc65ba285b24)

---
+++ src/main/java/org/example/filesave/XmlSerializer.java
@@ -0,0 +1,40 @@
+package org.example.filesave;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.lang.reflect.Type;
+
+public class XmlSerializer implements Serializer
+{
+    private static Logger log = LoggerFactory.getLogger(XmlSerializer.class.getSimpleName());
+    @Override
+    public <T> String Serialize(T elem) {
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        String res = "";
+        try {
+            res = xmlMapper.writeValueAsString(elem);
+        } catch (JsonProcessingException ex) {
+            log.error(ex.getMessage());
+        }
+        return res;
+    }
+
+    @Override
+    public <T> T Deserialize(String str, Type elementType) {
+        if (StringUtils.isBlank(str)) return null;
+        XmlMapper xmlMapper = new XmlMapper();
+        T res = null;
+        try {
+            res = xmlMapper.readValue(str, xmlMapper.constructType(elementType));
+        } catch (JsonProcessingException ex) {
+            log.error(ex.getMessage());
+            System.out.println(ex.getMessage());
+        }
+        return res;
+    }
+}
